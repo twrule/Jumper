036bebab2005d88bb97012076d47a9e48cd4025f
Tyler Rule <tylerwrule@gmail.com>
2020-10-19 21:20:43 -0700

    finished gameplay.  should style it a bit

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
 	const jumper = document.createElement('div');
 
 	let jumperLeftSpace = 50;
-	let jumperBottomSpace = 150;
+	let startPoint = 150
+	let jumperBottomSpace = startPoint;
 	let isGameOver = false;
 	let platformCount = 5;
 	let platforms = [];
 	let upTimerID;
 	let downTimerID;
+	let isJumping = true;
+	let isGoingLeft = false;
+	let isGoingRight = true;
+	let leftTimerID;
+	let rightTimerID;
+	let score = 0;
 
 	function createJumper() {
 		grid.appendChild(jumper);
 		jumper.classList.add('jumper');
-		jumperLeftSpace = platforms[0].left;
+		// Add 12 to center jumper(60px) on platform(85px)
+		jumperLeftSpace = platforms[0].left + 12;
 		jumper.style.left = jumperLeftSpace + 'px';
 		jumper.style.bottom = jumperBottomSpace + 'px';
 	}
@@ -47,10 +55,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function jump(){
 		clearInterval(downTimerID);
+		isJumping = true;
 		upTimerID = setInterval(function(){
 			jumperBottomSpace += 20;
 			jumper.style.bottom = jumperBottomSpace + 'px';
-			if(jumperBottomSpace > 350){
+			if(jumperBottomSpace > startPoint + 200){
 				fall();
 			}
 		}, 30)
@@ -58,6 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function fall(){
 		clearInterval(upTimerID);
+		isJumping = false;
 		downTimerID = setInterval(function(){
 			jumperBottomSpace -= 5;
 			jumper.style.bottom = jumperBottomSpace + 'px';
@@ -65,13 +75,32 @@ document.addEventListener('DOMContentLoaded', () => {
 			if(jumperBottomSpace <= 0){
 				gameOver();		
 			}
+
+			platforms.forEach(platform => {
+				if(
+					(jumperBottomSpace >= platform.bottom) &&
+					(jumperBottomSpace <= platform.bottom + 15) &&
+					((jumperLeftSpace + 60) >= platform.left) &&
+					(jumperLeftSpace <= (platform.left + 85)) &&
+					isJumping == false
+				){
+					startPoint = jumperBottomSpace;
+					jump();
+				}
+			})
 		}, 30);
 	}
 	
 	function gameOver(){
 		isGameOver = true;
+		while(grid.firstChild){
+			grid.removeChild(grid.firstChild);
+		}
+		grid.innerHTML = score;
 		clearInterval(upTimerID);
 		clearInterval(downTimerID);
+		clearInterval(leftTimerID);
+		clearInterval(rightTimerID);
 	}
 
 	function movePlatforms(){
@@ -82,8 +111,68 @@ document.addEventListener('DOMContentLoaded', () => {
 				let visual = platform.visual;
 				visual.style.bottom = platform.bottom + 'px';
 
+				if(platform.bottom < 10){
+					let firstPlatform = platforms[0].visual;
+					firstPlatform.classList.remove('platform');
+					platforms.shift();
+
+					// 600 is hardcode top of grids
+					let newPlatform = new Platform(600);
+					platforms.push(newPlatform);
+					score++;
+				}
 			})
 		}
+	}
+
+	function control(e){
+		if(e.key === "j" || e.key === "ArrowLeft"){
+			moveLeft();
+		}
+		else if(e.key === "l" || e.key === "ArrowRight"){
+			moveRight();
+		}
+		else if(e.key === "k" || e.key === "ArrowUp"){
+			moveStraight();
+		}
+	}
+
+	function moveLeft(){
+		if(isGoingRight){
+			clearInterval(rightTimerID);
+			isGoingRight = false;
+		}
+		isGoingLeft = true;
+		leftTimerID = setInterval(function(){
+			if(jumperLeftSpace >= 0){
+				jumperLeftSpace -= 5;
+				jumper.style.left = jumperLeftSpace + 'px';
+			}
+			else moveRight();
+			
+		}, 30)
+	}
+
+	function moveRight(){
+		if(isGoingRight){
+			clearInterval(leftTimerID);
+			isGoingLeft = false;
+		}
+		isGoingRight = true;
+		rightTimerID = setInterval(function() {
+			if(jumperLeftSpace <= 340){
+				jumperLeftSpace += 5;
+				jumper.style.left = jumperLeftSpace + 'px';
+			}
+			else moveLeft();
+		}, 30)
+	}
+
+	function moveStraight(){
+		isGoingRight = false;
+		isGoingLeft = false;
+		clearInterval(rightTimerID);
+		clearInterval(leftTimerID);
 	}
 
 	function start(){
@@ -92,6 +181,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			createJumper();
 			setInterval(movePlatforms, 30);
 			jump();
+			document.addEventListener('keyup', control);
 		}
 	}
 
